aa1628ca65a7769341444ba1c713c47d8d43c404
hania-jas <60855635+hania-jas@users.noreply.github.com>
2020-08-01 17:14:50 +0200

    added encoding function for caesar cipher

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const input = document.getElementById('userWordInput');
 const checkButton = document.getElementById('checkWord');
 const displayReversed = document.getElementById('displayReversed');
+const encodeButton = document.getElementById('encodeWord');
+const displayEncoded = document.getElementById('displayEncoded');
 
-const checkIfWordIsPalindrome = () => {
+const splitWordToLetters = () => {
     const inputValue = input.value;
     const charactersArr = inputValue.split('');
     console.log(charactersArr);
     console.log(inputValue);
+    return [charactersArr, inputValue];
+}
+
+const checkIfWordIsPalindrome = () => {
+    let returnedValuesFromSplitWordFunction = splitWordToLetters();
+    const charactersArr = returnedValuesFromSplitWordFunction[0];
     const reversedCharacters = [];
 
     for (let i = charactersArr.length - 1; i >= 0; i--) {
@@ -16,9 +24,8 @@ const checkIfWordIsPalindrome = () => {
     const reversedString = reversedCharacters.join('');
     console.log(reversedString);
 
-    if (inputValue === reversedString) {
+    if (returnedValuesFromSplitWordFunction[1] === reversedString) {
         alert('PALINDROME!');
-
     } else {
         alert('TRY AGAIN');
     }
@@ -26,4 +33,28 @@ const checkIfWordIsPalindrome = () => {
 
 }
 
+const encodeFunction = () => {
+    let returnedValuesFromSplitWordFunction = splitWordToLetters();
+    const alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż'];
+    const charactersArr = returnedValuesFromSplitWordFunction[0];
+    console.log(alphabet);
+    let encodedArr = [];
+
+    for (let i = 0; i < charactersArr.length; i++) {
+        console.log(charactersArr[i]);
+        for (let j = 0; j < alphabet.length; j++) {
+
+            if (charactersArr[i] === alphabet[j]) {
+                const tempVal = alphabet[j += 2];
+                console.log('tempVal', tempVal);
+                encodedArr.push(tempVal);
+            }
+        }
+        console.log('encodedArr', encodedArr);
+    }
+    const encodedString = encodedArr.join('');
+    displayEncoded.innerHTML = `ENCODED PHRASE: ${encodedString}`;
+}
+
 checkButton.addEventListener('click', checkIfWordIsPalindrome);
+encodeButton.addEventListener('click', encodeFunction);
